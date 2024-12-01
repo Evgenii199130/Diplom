e8fef3442df515864dff1fff6c0b9f91acaa4656
Evgenii199130 <igan199130@mail.ru>
2024-12-01 19:54:04 +0300

    добавление ответов

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -114,6 +114,34 @@ resource "yandex_compute_disk" "disk-zabbix" {
 
 }
 
+resource "yandex_compute_instance" "vm3-zabbix" {
+  name                      = "vm3-zabbix"
+  zone                      = "ru-central1-b" #c
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.disk-zabbix.id
+  }
+
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.public.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.inner.id, yandex_vpc_security_group.public-zabbix.id]
+    ip_address         = "10.0.4.5"
+  }
+
+metadata = {
+ssh-keys = "ubuntu:${local.ssh_key_default}"
+}
+}
+
+#---elastic---
 resource "yandex_compute_disk" "disk-elastic" {
   name     = "disk-elastic"
   type     = "network-hdd"
@@ -123,6 +151,26 @@ resource "yandex_compute_disk" "disk-elastic" {
 
 }
 
+resource "yandex_compute_instance" "vm5-elastic" {
+  name                      = "vm5-elastic"
+  zone                      = "ru-central1-b"
+
+  resources {
+    cores         = 2
+    memory        = 4
+    core_fraction = 20
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.disk-elastic.id
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.inner-services.id
+    security_group_ids = [yandex_vpc_security_group.inner.id]
+    ip_address         = "10.0.3.4"
+  }
+#---kibana---
 resource "yandex_compute_disk" "disk-kibana" {
   name     = "disk-n-kibana"
   type     = "network-hdd"
@@ -130,4 +178,30 @@ resource "yandex_compute_disk" "disk-kibana" {
   size     = 10
   image_id = "fd87j6d92jlrbjqbl32q"
 
+}
+
+resource "yandex_compute_instance" "vm6-kibana" {
+  name                      = "vm6-kibana"
+  hostname                  = "kibana"
+  zone                      = "ru-central1-b"
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.disk-kibana.id
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.public.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.inner.id, yandex_vpc_security_group.public-kibana.id]
+    ip_address         = "10.0.4.3"
+  }
+metadata = {
+ssh-keys = "ubuntu:${local.ssh_key_default}"
+}
 }
