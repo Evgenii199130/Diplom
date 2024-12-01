bfcec554944e78accbbe2c7648f4b987ec1c957e
Evgenii199130 <igan199130@mail.ru>
2024-12-01 20:34:02 +0300

    добавление ответов

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "external_ip_addres_load_balancer" {
   value = yandex_alb_load_balancer.new_lb.listener.0.endpoint.0.address.0.external_ipv4_address
 }
-output "bastion" {
+output "bastion-host" {
   value = yandex_compute_instance.bastion.network_interface.0.nat_ip_address
 }
-output "kibana" {
+output "vm6-kibana" {
   value = yandex_compute_instance.kibana.network_interface.0.nat_ip_address
 }
